5cee9f1d9d44105382647e56a266f41321b172e2
Stephane Courivaud <stephane.courivaud@gmail.com>
2015-02-16 22:25:52 +0100

    Modification du site mineure

--- partials/prestations.php
+++ partials/prestations.php
@@ -179,18 +179,6 @@
 					<div class="modal-body">
                         <ul class="list-unstyled">
                             <li><i class="fa fa-check"></i> Horaires flexibles du mardi au samedi</li>
-                            <li><i class="fa fa-check"></i> Honoraires
-                                <ul class="list-unstyled">
-                                    <li>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-right"></i> Bilan Nutritionnel : 60 €</li>
-                                    <li>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-right"></i> Consultation de suivi  : 50 €</li>
-                                    <li>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-right"></i> Forfaits Suivi Diététique :</li>
-                                    <ul class="list-unstyled">
-                                        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Bilan Nutritionnel + 3 consultations : 205 euros</li>
-                                        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Bilan Nutritionnel + 6 consultations : 350 euros</li>
-                                        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Bilan Nutritionnel + 10 consultations : 550 euros</li>
-                                    </ul>
-                                </ul>
-                            </li>
                             <li><i class="fa fa-check"></i> La consultation initiale permet de faire un bilan nutritionnel complet, connaitre vos attentes et vos motivations et de définir ensemble les objectifs de votre suivi</li>
                             <li><i class="fa fa-check"></i> Les consultations de suivi permettent d'évaluer l'efficacité de la prise en charge et de répondre à vos questions</li>
                             <li><i class="fa fa-check"></i> Pour un suivi efficace, les consultations doivent avoir lieu tous les 15 jours - 1 mois maximum</li>
